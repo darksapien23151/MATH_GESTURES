0e51ce9e5956d34cd5264586baecc64a0f289bb9
darksapien23151 <141660450+darksapien23151@users.noreply.github.com>
2024-06-21 16:33:01 +0530

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,6 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.image("image.png")
 
-# Create two columns
 col1, col2 = st.columns([2,1])
 with col1:
     run = st.checkbox("Run", value= True)
@@ -19,24 +18,18 @@ with col2:
         output_text_Area = st.subheader("")
 genai.configure(api_key="AIzaSyD66fk265CL6P9qhS7uUqHM8xBJBHdWtKo")
 model = genai.GenerativeModel('gemini-1.5-flash')
-# Initialize the webcam to capture video
-# The '2' indicates the third camera connected to your computer; '0' would usually refer to the built-in camera
 cap = cv2.VideoCapture(0)
 cap.set(3,1280)
 cap.set(4,720)
 
-# Initialize the HandDetector class with the given parameters
 detector = HandDetector(staticMode=False, maxHands=1, modelComplexity=1, detectionCon=0.7, minTrackCon=0.5)
 
 
 def getHandInfo(img):
     hands, img = detector.findHands(img, draw=False, flipType=True)
-    # Check if any hands are detected
     if hands:
-        # Information for the first hand detected
-        hand = hands[0]  # Get the first hand detected
-        lmList = hand["lmList"]  # List of 21 landmarks for the first hand
-        # Count the number of fingers up for the first hand
+        hand = hands[0] 
+        lmList = hand["lmList"]
         fingers = detector.fingersUp(hand)
         print(fingers)
         return fingers, lmList
@@ -74,10 +67,7 @@ output_text = ""
 
    
 
-# Continuously get frames from the webcam
 while True:
-    # Capture each frame from the webcam
-    # 'success' will be True if the frame is successfully captured, 'img' will contain the frame
     success, img = cap.read()
     img = cv2.flip(img, 1)
 
@@ -96,20 +86,14 @@ while True:
 
     if output_text:
         output_text_Area.text(output_text)
-    # Display the image in a window
-   # cv2.imshow("Image", img)
-   # cv2.imshow("Canvas", canvas)
-   # cv2.imshow("image_combined", image_combined)
 
-    # Keep the window open and update it for each frame; wait for 1 millisecond between frames
+
     k = cv2.waitKey(1)
     if cv2.waitKey(1) & k == ord('q'):
       break
 
-# Release the webcam resource
 cap.release()
 
-# Close any OpenCV windows
 cv2.destroyAllWindows()
 
  
